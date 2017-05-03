d27c5d7a2cd2c58762afa5055f2d3f8e1ce58fdc
Jaewon Jeong <7904001@gmail.com>
2017-05-02 17:04:01 -0700

    Added subtotal/total
    added a function to calculate the subtotal/total

--- food-truck.php
+++ food-truck.php
@@ -1,4 +1,14 @@
 <style>
+	@import url('https://fonts.googleapis.com/css?family=Roboto:300,400');
+	
+	body {
+		font-family: 'Roboto', sans-serif;
+	}
+	
+	h2,h3,h4,h5,h6 {
+		font-weight: 400;
+	}
+	
 	input[type=number] {
 		width: 50px;
 	} 
@@ -16,35 +26,46 @@ if (isset($_REQUEST['act'])) {
 }
 
 switch ($myAction) { // check 'act' for type of process
-	case "display": # 2)Display user's name!
+	case "display": #2)Display what user ordered
 	 	showData();
 	 	break;
-	default: # 1)Ask user to enter their name 
+	default: #1)Ask user to place an order
 	 	showForm();
 }
 
-function showForm() { // shows form so user can enter their name.  Initial scenario
+function showForm() { // shows form so user can place an order
 	global $config;
-	
 	
 	echo '<form action="'.THIS_PAGE.'" method="post">';
 	foreach ($config->items as $item) {
 		//echo "<p>ID:$item->ID  Name:$item->Name</p>"; 
 		//echo '<p>Taco <input type="text" name="item_1" /></p>';
-		echo '<p>' . $item->name . ' <input type="number" min="0" max="10" name="item_' . $item->id . '"/> ' . $item->price . '</p>';
+		echo '<h2>' . $item->name . '</h2>';
+		echo '<h5>Price: $' . $item->price . '</h5>';
+//		echo '<h5>Quantity: <input type="number" min="0" max="10" name="item_' . $item->id . '"/></h5>';
+		
+		echo '<select name="item_'.$item->id.'">';
+			for ($i = 0; $i <= 10; $i++) {
+				echo '<option value="'.$i.'">'.$i.'</option>';
+			}
+		echo '</select>';
+
+
+		echo '<p>' . $item->description . '</p>';
+		
+		
+		/*  if the array of extras is greater than zero, 
+			consider adding a new set of elements, i.e. checkboxes 
+			checkboxes
+			<checkbox name="extra_1" value="cheese">Cheese<br /> */
 		if (count($item->extras) > 0) {
+			echo '<p>Add your choice of toppings.</p>';
 			foreach ($item->extras as $extra) {
 				echo '<input type="checkbox" name="extra_" />' . $extra . '<br />';
 			}
 		}
-		/* 
-		if the array of extras is greater than zero, 
-		consider adding a new set of elements, i.e. checkboxes 
-		checkboxes
-		<checkbox name="extra_1" value="cheese">Cheese<br />
-
-		*/
-	}       
+		echo '<hr />';
+	}   
 	echo '	
 			<input type="submit" value="Order">
 			<input type="hidden" name="act" value="display" />
@@ -52,12 +73,12 @@ function showForm() { // shows form so user can enter their name.  Initial scena
 	';
 }
 
-function showData() { #form submits here we show entered name
-	
-    //dumpDie($_POST);
-	foreach($_POST as $name => $value) { //loop the form elements
-        
-        //if form name attribute starts with 'item_', process it
+function showData() { // shows what user submitted
+//    echo '<pre>';
+// 	  dumpdie($_POST);
+//    echo '</pre>';
+	foreach($_POST as $name => $value) { // loop the form elements
+        // if form name attribute starts with 'item_', process it
         if (substr($name, 0, 5) == 'item_') {
             // explode the string into an array on the "_"
             $name_array = explode('_', $name);
@@ -80,14 +101,33 @@ function showData() { #form submits here we show entered name
 				and create subtotals, etc.
 			
 			*/
+			$thisItem = getItem($id, $value);
+
             if ($value > 0) { 
-				echo "<p>You ordered $value of item number $id</p>";
-			} 
+//				echo '<pre>';
+//				var_dump ($items[1]);
+//				echo '</pre>';
+				echo $thisItem;
+			}
         }
     }	
-	echo '<p><a href="' . THIS_PAGE . '">RESET</a></p>';
+	echo '<p><a href="' . THIS_PAGE . '">Clear</a></p>';
 }
 
+
+function getItem($id, $value) {
+	global $config;
+
+	$itemName = $config->items[$id - 1]->name;
+	$itemPrice = $config->items[$id - 1]->price;
+	$subtotal = $value * (float)$itemPrice;
+	$total = round(($subtotal * 1.101), 2); // 10.1% Seattle sales tax
+	
+	return "
+		<p>You ordered $value $itemName.</p>
+		<p>Your subtotal is $$subtotal.</p>
+		<p>Your total after tax is $$total.</p>";
+}
 
 
 ?>
