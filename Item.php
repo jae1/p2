28baf0d6a5a930d47bbb4fa7db014551922c7382
kdaniel32 <kevindaniel14@outlook.com>
2017-05-04 08:39:54 -0700

    Adding comments and removing code duplicated in items.php

--- Item.php
+++ Item.php
@@ -1,52 +1,23 @@
 <?php
+//Item.php
+
 /**
-* Class Item - Stores menu items
-*
-* More stuff about the class
-*
-*<code>
-* $myItem = new Item($ID,$Name,$Description,$Price);
-* $myItem->addExtra("Name of extra");
-*</code>
-*
-* @author
-* @see 
-* @todo none
-*/
-
-//items4.php
-
-$myItem = new Item(1,"Taco","Our Tacos are awesome!",4.95);
-$myItem->addExtra("Sour Cream");
-$myItem->addExtra("Cheese");
-$myItem->addExtra("Guacamole");
-$config->items[] = $myItem;
-
-$myItem = new Item(2,"Sundae","Our Sundaes are awesome!",3.95);
-$myItem->addExtra("Sprinkles");
-$myItem->addExtra("Chocolate Sauce");
-$myItem->addExtra("Nuts");
-$config->items[] = $myItem;
-
-$myItem = new Item(3,"Salad","Our Salads are awesome!",5.95);
-$myItem->addExtra("Croutons");
-$myItem->addExtra("Bacon");
-$myItem->addExtra("Lemon Wedges");
-$myItem->addExtra("Avacado");
-$config->items[] = $myItem;
-
-
-//create a counter to load the ids...
-//$items[] = new Item(1,"Taco","Our Tacos are awesome!",4.95);
-//$items[] = new Item(2,"Sundae","Our Sundaes are awesome!",3.95);
-//$items[] = new Item(3,"Salad","Our Salads are awesome!",5.95);
-
-/*
-echo '<pre>';
-var_dump($items);
-echo '</pre>';
-die;
-*/
+ * Class Item - Stores menu items
+ *
+ * More stuff about the class
+ *
+ *<code>
+ * $myItem = new Item($ID,$Name,$Description,$Price);
+ * $myItem->addExtra("Name of extra");
+ *</code>
+ *
+ * @author Mariam Abdelmohsen
+ * @author Amy Bartolotta
+ * @author Kevin Daniel
+ * @author Jaewon Jeong
+ * @see 
+ * @todo none
+ */
 
 
 class Item
@@ -73,14 +44,3 @@ class Item
     }#end addExtra()
 
 }#end Item class
-
-
-
-
-
-
-
-
-
-
-
