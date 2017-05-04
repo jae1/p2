f3e33d7ae307170e7e192bd0df8c8baf18bff311
kdaniel32 <kevindaniel14@outlook.com>
2017-05-04 14:35:21 -0700

    changing menu items

--- items.php
+++ items.php
@@ -3,71 +3,27 @@
 
 require 'Item.php'; //kd - require the Item class file
 
-$myItem = new Item(1,"Taco","Our Tacos are awesome!",4.95);
+$myItem = new Item(1, "Taco", "Our Tacos are awesome!", 1.99);
 $myItem->addExtra("Sour Cream");
+$myItem->addExtra("Reduced Fat Sour Cream");
 $myItem->addExtra("Cheese");
+$myItem->addExtra("Nacho Cheese Sauce");
+$myItem->addExtra("Chipotle Sauce");
+$myItem->addExtra("Pico de Gallo");
 $myItem->addExtra("Guacamole");
 $config->items[] = $myItem;
-
-$myItem = new Item(2,"Sundae","Our Sundaes are awesome!",3.95);
-$myItem->addExtra("Sprinkles");
-$myItem->addExtra("Chocolate Sauce");
-$myItem->addExtra("Nuts");
+$myItem = new Item(2,"Burrito","Our Burritos are awesome!",3.95);
+$myItem->addExtra("Sour Cream");
+$myItem->addExtra("Reduced Fat Sour Cream");
+$myItem->addExtra("Cheese");
+$myItem->addExtra("Nacho Cheese Sauce");
+$myItem->addExtra("Chipotle Sauce");
+$myItem->addExtra("Pico de Gallo");
+$myItem->addExtra("Guacamole");
 $config->items[] = $myItem;
-
 $myItem = new Item(3,"Salad","Our Salads are awesome!",5.95);
 $myItem->addExtra("Croutons");
 $myItem->addExtra("Bacon");
 $myItem->addExtra("Lemon Wedges");
 $myItem->addExtra("Avacado");
 $config->items[] = $myItem;
-
-
-//create a counter to load the ids...
-//$items[] = new Item(1,"Taco","Our Tacos are awesome!",4.95);
-//$items[] = new Item(2,"Sundae","Our Sundaes are awesome!",3.95);
-//$items[] = new Item(3,"Salad","Our Salads are awesome!",5.95);
-
-/*
-echo '<pre>';
-var_dump($items);
-echo '</pre>';
-die;
-*/
-
-/* kd - class definition moved to Item.php
-class Item
-{
-    public $ID = 0;
-    public $Name = '';
-    public $Description = '';
-    public $Price = 0;
-    public $Extras = array();
-    
-    public function __construct($ID,$Name,$Description,$Price)
-    {
-        $this->ID = (int)$ID; //kd - cast to integer
-        $this->Name = $Name;
-        $this->Description = $Description;
-        $this->Price = (float)$Price; //kd - cast to float
-        
-    }#end Item constructor
-    
-    public function addExtra($extra)
-    {
-        $this->Extras[] = $extra;
-        
-    }#end addExtra()
-
-}#end Item class
-
-*/
-
-
-
-
-
-
-
-
-
